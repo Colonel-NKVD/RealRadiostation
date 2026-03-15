2b7b9cf05b4d10e8c864cf897392433efac2e386
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-15 18:02:09 +0300

    Create CommandRadio.cs

---
+++ CommandRadio.cs
@@ -0,0 +1,54 @@
+using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using UnityEngine;
+using SDG.Unturned;
+
+namespace RealRadiostation
+{
+    public class CommandRadio : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "radio";
+        public string Help => "Устанавливает частоту ближайшей радиостанции";
+        public string Syntax => "<frequency>";
+        public List<string> Aliases => new List<string> { "freq" };
+        public List<string> Permissions => new List<string> { "realradio.admin" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+
+            if (command.Length != 1 || !uint.TryParse(command[0], out uint newFreq))
+            {
+                UnturnedChat.Say(player, "Использование: /radio <частота>", Color.red);
+                return;
+            }
+
+            // Ищем ближайшую радиостанцию в небольшом радиусе
+            RadioStationComponent nearestStation = null;
+            float minDistance = 3f; 
+
+            foreach (var station in RealRadiostationPlugin.Instance.ActiveStations)
+            {
+                float dist = Vector3.Distance(player.Position, station.transform.position);
+                if (dist < minDistance)
+                {
+                    minDistance = dist;
+                    nearestStation = station;
+                }
+            }
+
+            if (nearestStation != null)
+            {
+                nearestStation.Frequency = newFreq;
+                UnturnedChat.Say(player, $"Частота радиостанции установлена на: {newFreq} MHz", Color.cyan);
+            }
+            else
+            {
+                UnturnedChat.Say(player, "Рядом не найдено активных радиостанций!", Color.red);
+            }
+        }
+    }
+}
