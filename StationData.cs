16c16c021ea4ceb5330e3905ef4869cd2df3e3e5
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-15 18:05:56 +0300

    Create StationData.cs

---
+++ StationData.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RealRadiostation
+{
+    public class StationData
+    {
+        public uint Frequency { get; set; }
+        public RadioMode Mode { get; set; }
+    }
+
+    public static class DataStorage
+    {
+        private static string Path => "Plugins/RealRadiostation/StationsData.json";
+
+        public static void Save(Dictionary<ulong, StationData> data)
+        {
+            File.WriteAllText(Path, JsonConvert.SerializeObject(data, Formatting.Indented));
+        }
+
+        public static Dictionary<ulong, StationData> Load()
+        {
+            if (!File.Exists(Path)) return new Dictionary<ulong, StationData>();
+            return JsonConvert.DeserializeObject<Dictionary<ulong, StationData>>(File.ReadAllText(Path));
+        }
+    }
+}
