bb5d0dd03552d43fd67f90cc01b09ea47ec4743b
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-16 13:38:23 +0300

    Update VoicePatch.cs

--- VoicePatch.cs
+++ VoicePatch.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 namespace RealRadiostation
 {
-    // Nelson (разработчик Unturned) в новых версиях переименовал метод. 
-    // Пробуем пропатчить handleRelayVoiceInternal
-    [HarmonyPatch(typeof(PlayerVoice), "handleRelayVoiceInternal")]
     public static class VoicePatch
     {
-        [HarmonyPrefix]
-        public static bool Prefix(PlayerVoice __instance, bool wantsToUseRadio, ref bool shouldAllow, ref bool shouldBroadcastOverRadio)
+        // Используем object[] __args, чтобы патч подошел к любой версии Unturned
+        public static bool Prefix(PlayerVoice __instance, object[] __args)
         {
+            // В Unturned параметры обычно такие: 0: wantsToUseRadio, 1: shouldAllow, 2: shouldBroadcast
+            // Мы безопасно достаем первый параметр (нажата ли кнопка рации)
+            bool wantsToUseRadio = (bool)__args[0];
+
             if (!wantsToUseRadio)
             {
                 var plugin = RealRadiostationPlugin.Instance;
                 if (plugin == null || __instance.player == null) return true;
 
-                // Проверяем наличие радиостанции рядом
                 var station = plugin.GetNearestStation(__instance.player.transform.position, plugin.Configuration.Instance.TransmitRadius);
                 
                 if (station != null && station.Mode == RadioMode.TransmitAndListen)
                 {
-                    // Настраиваем рацию игрока на частоту станции
                     __instance.player.quests.sendSetRadioFrequency((uint)station.Frequency);
                     
-                    shouldAllow = true;
-                    shouldBroadcastOverRadio = true; 
+                    // Меняем значения параметров "разрешить" и "вещать через рацию" в массиве аргументов
+                    __args[1] = true; // shouldAllow
+                    __args[2] = true; // shouldBroadcastOverRadio
                 }
             }
-            return true; // Разрешаем игре выполнить остальную логику
+            return true;
         }
     }
 }
