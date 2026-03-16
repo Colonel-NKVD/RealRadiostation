97677e3efb5e826b83a0ab05d8d1b515f00da20d
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-16 19:41:36 +0300

    Update VoicePatch.cs

--- VoicePatch.cs
+++ VoicePatch.cs
@@ -1,140 +1,57 @@
-using Rocket.Core.Plugins;
+using HarmonyLib;
 using SDG.Unturned;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using HarmonyLib;
 using System.Reflection;
-using Logger = Rocket.Core.Logging.Logger;
 
 namespace RealRadiostation
 {
-    public class RealRadiostationPlugin : RocketPlugin<RealRadiostationConfig>
+    public static class VoicePatch
     {
-        public static RealRadiostationPlugin Instance;
-        public List<RadioStationComponent> ActiveStations = new List<RadioStationComponent>();
-        private Harmony harmony;
-
-        protected override void Load()
+        public static bool Prefix(PlayerVoice __instance, MethodBase __originalMethod, object[] __args)
         {
-            Instance = this;
-            Logger.Log("-----------------------------------------------");
-            Logger.Log("[RealRadiostation] ВОССТАНОВЛЕНИЕ И ДИАГНОСТИКА...");
+            // ЛОГ №1: Фиксируем любое использование микрофона
+            Rocket.Core.Logging.Logger.Log($"[DETECTOR] Вызван метод: {__originalMethod.Name} от игрока {__instance.player.channel.owner.playerID.characterName}");
 
-            harmony = new Harmony("com.realradiostation.patch");
+            var player = __instance.player;
+            if (RealRadiostationPlugin.Instance == null) return true;
 
-            // Список методов для поиска рабочего захвата голоса
-            string[] voiceMethods = { "askVoiceChat", "receiveVoiceChat", "handleVoiceChat", "ReceiveVoice" };
-            
-            int patchCount = 0;
-            foreach (var name in voiceMethods)
+            // Тестовый радиус 10 метров
+            float radius = 10f; 
+            var station = RealRadiostationPlugin.Instance.GetNearestStation(player.transform.position, radius);
+
+            if (station != null)
             {
-                var method = typeof(PlayerVoice).GetMethod(name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                if (method != null)
+                Rocket.Core.Logging.Logger.Log($"[DETECTOR] Игрок РЯДОМ со станцией {station.Frequency}");
+
+                if (__args != null)
                 {
-                    var prefix = typeof(VoicePatch).GetMethod("Prefix", BindingFlags.Static | BindingFlags.Public);
-                    harmony.Patch(method, new HarmonyMethod(prefix));
-                    Logger.Log($"[DEBUG] Патч установлен на: {name}");
-                    patchCount++;
-                }
-            }
-
-            if (patchCount == 0) Logger.LogError("[FATAL] Ни один метод голоса не найден!");
-
-            Level.onPostLevelLoaded += OnLevelLoaded;
-            BarricadeManager.onBarricadeSpawned += OnBarricadeSpawned;
-            
-            if (Level.isLoaded) ScanStations();
-
-            Logger.Log("[RealRadiostation] Плагин готов к работе.");
-            Logger.Log("-----------------------------------------------");
-        }
-
-        private void OnLevelLoaded(int level) => ScanStations();
-        private void OnBarricadeSpawned(BarricadeRegion region, BarricadeDrop drop) 
-        { 
-            if (drop.asset.id == 1466) ScanStations(); 
-        }
-
-        public void ScanStations()
-        {
-            ActiveStations.Clear();
-            if (BarricadeManager.regions == null) return;
-
-            var savedData = DataStorage.Load();
-
-            foreach (var region in BarricadeManager.regions)
-            {
-                foreach (var drop in region.drops)
-                {
-                    if (drop.asset.id == 1466)
+                    for (int i = 0; i < __args.Length; i++)
                     {
-                        var comp = drop.model.gameObject.GetComponent<RadioStationComponent>() 
-                                   ?? drop.model.gameObject.AddComponent<RadioStationComponent>();
-                        
-                        string hash = GetPosHash(drop.model.position);
-                        
-                        if (savedData != null && savedData.ContainsKey(hash))
+                        if (__args[i] is bool)
                         {
-                            comp.Frequency = savedData[hash].Frequency;
-                            comp.Mode = savedData[hash].Mode;
+                            __args[i] = true; 
+                            Rocket.Core.Logging.Logger.Log($"[DETECTOR] Аргумент {i} (bool) изменен на TRUE");
                         }
-                        else
-                        {
-                            comp.Frequency = 111111; 
-                            comp.Mode = RadioMode.TransmitAndListen;
-                        }
-
-                        if (!ActiveStations.Contains(comp)) ActiveStations.Add(comp);
                     }
                 }
-            }
-            Logger.Log($"[DEBUG] Активных станций в списке: {ActiveStations.Count}");
-        }
 
-        // ТОТ САМЫЙ МЕТОД, КОТОРОГО НЕ ХВАТАЛО
-        public void SaveAllStations()
-        {
-            var dict = new Dictionary<string, StationData>();
-            foreach (var s in ActiveStations)
+                player.quests.sendSetRadioFrequency(station.Frequency);
+            }
+            else
             {
-                if (s != null)
+                // Если станций нет рядом, проверим, есть ли они вообще в мире
+                if (RealRadiostationPlugin.Instance.ActiveStations.Count > 0)
                 {
-                    dict[GetPosHash(s.transform.position)] = new StationData 
-                    { 
-                        Frequency = s.Frequency, 
-                        Mode = s.Mode 
-                    };
+                    float d = Vector3.Distance(player.transform.position, RealRadiostationPlugin.Instance.ActiveStations[0].transform.position);
+                    Rocket.Core.Logging.Logger.Log($"[DETECTOR] Станция 1466 найдена, но она далеко: {d:F1}м");
+                }
+                else
+                {
+                    Rocket.Core.Logging.Logger.Log("[DETECTOR] Станций с ID 1466 в активном списке нет.");
                 }
             }
-            DataStorage.Save(dict);
-            Logger.Log($"[DEBUG] Состояние {dict.Count} станций сохранено в JSON.");
-        }
 
-        public string GetPosHash(Vector3 pos) => $"{pos.x:F1}_{pos.y:F1}_{pos.z:F1}";
-
-        public RadioStationComponent GetNearestStation(Vector3 pos, float radius)
-        {
-            RadioStationComponent nearest = null;
-            float minSqrDist = radius * radius;
-            foreach (var s in ActiveStations)
-            {
-                if (s == null) continue;
-                float sqrDist = (s.transform.position - pos).sqrMagnitude;
-                if (sqrDist < minSqrDist)
-                {
-                    minSqrDist = sqrDist;
-                    nearest = s;
-                }
-            }
-            return nearest;
-        }
-
-        protected override void Unload()
-        {
-            if (harmony != null) harmony.UnpatchAll("com.realradiostation.patch");
-            ActiveStations.Clear();
-            Instance = null;
+            return true;
         }
     }
 }
