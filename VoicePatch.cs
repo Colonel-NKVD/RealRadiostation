fb97b4cfea0ed10b8428ec145ebc954261a9a261
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-16 19:38:04 +0300

    Update VoicePatch.cs

--- VoicePatch.cs
+++ VoicePatch.cs
@@ -9,13 +9,13 @@ namespace RealRadiostation
     {
         public static bool Prefix(PlayerVoice __instance, MethodBase __originalMethod, object[] __args)
         {
-            // ЛОГ №1: Мы вообще зашли в какой-либо метод?
+            // ЛОГ №1: Фиксируем любое использование микрофона
             Rocket.Core.Logging.Logger.Log($"[DETECTOR] Вызван метод: {__originalMethod.Name} от игрока {__instance.player.channel.owner.playerID.characterName}");
 
             var player = __instance.player;
             if (RealRadiostationPlugin.Instance == null) return true;
 
-            // Проверка дистанции (увеличим для теста до 10 метров)
+            // Тестовый радиус 10 метров
             float radius = 10f; 
             var station = RealRadiostationPlugin.Instance.GetNearestStation(player.transform.position, radius);
 
@@ -23,29 +23,31 @@ namespace RealRadiostation
             {
                 Rocket.Core.Logging.Logger.Log($"[DETECTOR] Игрок РЯДОМ со станцией {station.Frequency}");
 
-                // Пытаемся принудительно включить рацию в аргументах
                 if (__args != null)
                 {
                     for (int i = 0; i < __args.Length; i++)
                     {
                         if (__args[i] is bool)
                         {
-                            __args[i] = true; // Подменяем обычный голос на радио
-                            Rocket.Core.Logging.Logger.Log($"[DETECTOR] Аргумент {i} изменен на TRUE (Радио)");
+                            __args[i] = true; 
+                            Rocket.Core.Logging.Logger.Log($"[DETECTOR] Аргумент {i} (bool) изменен на TRUE");
                         }
                     }
                 }
 
-                // Силовое переключение частоты
                 player.quests.sendSetRadioFrequency(station.Frequency);
             }
             else
             {
-                // Если станция не найдена, пишем дистанцию до первой попавшейся для дебага
+                // Если станций нет рядом, проверим, есть ли они вообще в мире
                 if (RealRadiostationPlugin.Instance.ActiveStations.Count > 0)
                 {
                     float d = Vector3.Distance(player.transform.position, RealRadiostationPlugin.Instance.ActiveStations[0].transform.position);
-                    Rocket.Core.Logging.Logger.Log($"[DETECTOR] Станция далеко. Дистанция: {d:F1}м (нужно < {radius}м)");
+                    Rocket.Core.Logging.Logger.Log($"[DETECTOR] Станция 1466 найдена, но она далеко: {d:F1}м");
+                }
+                else
+                {
+                    Rocket.Core.Logging.Logger.Log("[DETECTOR] Станций с ID 1466 в активном списке нет.");
                 }
             }
 
