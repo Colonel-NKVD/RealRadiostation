078a97bf9e9aac5060538d32da9eabe41db9f58a
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-16 16:00:32 +0300

    Update CommandRadio.cs

--- CommandRadio.cs
+++ CommandRadio.cs
@@ -1,48 +1,79 @@
 using Rocket.API;
 using Rocket.Unturned.Chat;
-using Rocket.Unturned.Player;
-using System.Collections.Generic;
 using UnityEngine;
-using SDG.Unturned;
-using System.Globalization;
+using System.Collections.Generic;
 
 namespace RealRadiostation
 {
-    public class CommandRadio : IRocketCommand
+    public class CommandRadioFreq : IRocketCommand
     {
         public AllowedCaller AllowedCaller => AllowedCaller.Player;
-        public string Name => "radio";
-        public string Help => "Устанавливает частоту ближайшей радиостанции";
-        public string Syntax => "<frequency>";
-        public List<string> Aliases => new List<string> { "freq" };
-        public List<string> Permissions => new List<string> { "realradio.admin" };
+        public string Name => "radiofreq";
+        public string Help => "Сменить волну ближайшей радиостанции";
+        public string Syntax => "/radiofreq <волна>";
+        public List<string> Aliases => new List<string>();
+        public List<string> Permissions => new List<string> { "realradiostation.radiofreq" };
 
         public void Execute(IRocketPlayer caller, string[] command)
         {
-            UnturnedPlayer player = (UnturnedPlayer)caller;
-
-            // Используем float для поддержки дробных частот и обрабатываем разделители (точка/запятая)
-            if (command.Length != 1 || !float.TryParse(command[0].Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out float newFreq))
+            if (command.Length < 1)
             {
-                UnturnedChat.Say(player, "Использование: /radio <частота> (например, 111.111)", Color.red);
+                UnturnedChat.Say(caller, "Используйте: /radiofreq <число>", Color.red);
                 return;
             }
 
-            // Ищем ближайшую радиостанцию
-            RadioStationComponent nearestStation = RealRadiostationPlugin.Instance.GetNearestStation(player.Position);
+            var player = (Rocket.Unturned.Player.UnturnedPlayer)caller;
+            var plugin = RealRadiostationPlugin.Instance;
+            var station = plugin.GetNearestStation(player.Position, plugin.Configuration.Instance.TransmitRadius);
 
-            if (nearestStation != null)
+            if (station == null)
             {
-                nearestStation.Frequency = newFreq;
-                // Сохраняем изменения в файл StationsData.json
-                RealRadiostationPlugin.Instance.SaveStations(); 
-                
-                UnturnedChat.Say(player, $"Частота радиостанции установлена на: {newFreq:F3} MHz", Color.cyan);
+                UnturnedChat.Say(caller, "Вы не находитесь рядом с радиостанцией!", Color.red);
+                return;
+            }
+
+            // Умный парсер частоты (убирает точки и запятые)
+            string rawInput = command[0].Replace(".", "").Replace(",", "");
+            
+            if (uint.TryParse(rawInput, out uint newFreq))
+            {
+                station.Frequency = newFreq;
+                plugin.SaveAllStations();
+                UnturnedChat.Say(caller, $"Волна станции успешно изменена на {newFreq}", Color.green);
             }
             else
             {
-                UnturnedChat.Say(player, "Рядом не найдено активных радиостанций!", Color.red);
+                UnturnedChat.Say(caller, "Ошибка: Волна должна быть числом (например, 111.111)", Color.red);
             }
+        }
+    }
+
+    public class CommandRadioMode : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "radiomode";
+        public string Help => "Переключить режим радиостанции";
+        public string Syntax => "/radiomode";
+        public List<string> Aliases => new List<string>();
+        public List<string> Permissions => new List<string> { "realradiostation.radiomode" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            var player = (Rocket.Unturned.Player.UnturnedPlayer)caller;
+            var plugin = RealRadiostationPlugin.Instance;
+            var station = plugin.GetNearestStation(player.Position, plugin.Configuration.Instance.TransmitRadius);
+
+            if (station == null)
+            {
+                UnturnedChat.Say(caller, "Вы не находитесь рядом с радиостанцией!", Color.red);
+                return;
+            }
+
+            station.Mode = station.Mode == RadioMode.ListenOnly ? RadioMode.TransmitAndListen : RadioMode.ListenOnly;
+            plugin.SaveAllStations();
+            
+            string modeText = station.Mode == RadioMode.TransmitAndListen ? "Прием и Передача" : "Только Прием";
+            UnturnedChat.Say(caller, $"Режим станции изменен на: {modeText}", Color.green);
         }
     }
 }
