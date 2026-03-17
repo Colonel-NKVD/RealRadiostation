3c66969d86157a725bb42941bf6d69599ee5bfc4
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-17 15:25:34 +0300

    Update StationData.cs

--- StationData.cs
+++ StationData.cs
@@ -1,28 +1,29 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
 
 namespace RealRadiostation
 {
-    public enum RadioMode { ListenOnly, TransmitAndListen }
-    public class StationData { public uint Frequency { get; set; } public RadioMode Mode { get; set; } }
-    public class RadioStationComponent : MonoBehaviour { public uint Frequency; public RadioMode Mode; }
+    public class StationData
+    {
+        public uint Frequency { get; set; }
+        public bool CanTransmit { get; set; }
+    }
 
     public static class DataStorage
     {
-        private static string Path => "Plugins/RealRadiostation/StationsData.json";
-        public static void Save(Dictionary<string, StationData> data)
-        {
-            string dir = System.IO.Path.GetDirectoryName(Path);
-            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
-            File.WriteAllText(Path, JsonConvert.SerializeObject(data, Formatting.Indented));
-        }
+        private static string FilePath => Path.Combine(RealRadiostationPlugin.Instance.Directory, "StationsData.json");
+
         public static Dictionary<string, StationData> Load()
         {
-            if (!File.Exists(Path)) return new Dictionary<string, StationData>();
-            try { return JsonConvert.DeserializeObject<Dictionary<string, StationData>>(File.ReadAllText(Path)); }
-            catch { return new Dictionary<string, StationData>(); }
+            if (!File.Exists(FilePath)) return new Dictionary<string, StationData>();
+            string json = File.ReadAllText(FilePath);
+            return JsonConvert.DeserializeObject<Dictionary<string, StationData>>(json) ?? new Dictionary<string, StationData>();
+        }
+
+        public static void Save(Dictionary<string, StationData> data)
+        {
+            File.WriteAllText(FilePath, JsonConvert.SerializeObject(data, Formatting.Indented));
         }
     }
 }
